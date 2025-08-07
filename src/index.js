d872be4f4c3dad22eea1e279c79d364c42c7ee05
Borys Ziukov <ziukoff1985@gmail.com>
2025-08-07 19:40:14 +0300

    Add JS logic

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 'use strict';
 
-outer: for (let i = 2; i <= 100; i++) {
-    for (let j = 2; j < i; j++) {
-        if (i % j === 0) {
-            continue outer;
-        }
-    }
-    console.log(i);
+let totalVolume = 0;
+
+const wallThickness = 0.01;
+
+const pi = Math.PI;
+
+let currentInternalRadius = 0.24;
+
+for (let i = 1; i <= 12; i++) {
+    const currentVolume = (4 / 3) * pi * currentInternalRadius ** 3;
+
+    totalVolume += currentVolume;
+
+    currentInternalRadius += wallThickness;
 }
+
+console.log(
+    `Суммарный внутренний объем 12-ти шаров равен ${totalVolume.toFixed(2)} м3`
+);
